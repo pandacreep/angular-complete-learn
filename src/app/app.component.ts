8b873ceaae92d3e2cd165bfbf7d4a749ab9de7be
dmitriy.pak <dmt.alfa@gmail.com>
2024-12-30 15:00:39 +0500

    adding more components to the Demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
