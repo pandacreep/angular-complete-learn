bd178ad3d0f1fcc1435632aa69326c50499a7d9d
dmitriy.pak <dmt.alfa@gmail.com>
2025-01-12 11:24:41 +0500

    bootstrapping apps with angular modules (with error)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
