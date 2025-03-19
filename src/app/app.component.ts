0b7e98d1c25f0e4eb14c8d1db722920be913d191
Dmitriy Pak <dmt.alfa@gmail.com>
2025-03-19 10:17:40 +0500

    Component Lifecycle - a deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { LifecycleComponent } from "./lifecycle/lifecycle.component";
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [LifecycleComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  lifecycleComponentIsVisible = false;
+  lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+
+  onToggleLifecycleComponentVisibility() {
+    this.lifecycleComponentIsVisible = !this.lifecycleComponentIsVisible;
+  }
+
+  onChangeLifecycleInputText() {
+    this.lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+  }
+}
