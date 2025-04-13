d28c7d9254e2ad0c45f3095610f8cdcde5963603
dmitriy.pak <dmt.alfa@gmail.com>
2025-04-13 13:43:29 +0500

    a closer look at template for loops

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -14,6 +14,8 @@ import { ControlComponent } from "../../../shared/control/control.component";
 export class NewTicketComponent implements OnInit, AfterViewInit {
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // @Output() add = new EventEmitter<{title: string; text: string}>();
+  add = output<{title: string; text: string}>();
 
   ngOnInit() {
     console.log('ON INIT');
@@ -25,9 +27,8 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
     console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title: String, ticketText: String) {
-    console.log(title);
-    console.log(ticketText);
+  onSubmit(title: string, ticketText: string) {
+    this.add.emit({ title: title, text: ticketText })
     this.form?.nativeElement.reset();
   }
 }
