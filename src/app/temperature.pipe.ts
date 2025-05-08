cb0d7fab0b0e810b15539001cfd91eada0326a09
Dmitriy Pak <dmt.alfa@gmail.com>
2025-05-08 09:02:01 +0500

    changing pipes & being aware of limitations

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,10 +6,14 @@ import { output, Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
   transform(
-    value: string | number,
+    value: string | number | null,
     inputType: 'cel' | 'fah',
     outputType?: 'cel' | 'fah'
   ) {
+    if (!value) {
+      return value;
+    }
+
     let val: number;
 
     if (typeof value === 'string') {
@@ -18,22 +22,22 @@ export class TemperaturePipe implements PipeTransform {
       val = value;
     }
 
-	let outputTemp;
-	if (inputType === 'cel' && outputType === 'fah') {
-		outputTemp = val * (9 / 5) + 32;
-	} else if (inputType === 'fah' && outputType === 'cel') {
-		outputTemp = (val - 32) * (5 / 9);
-	} else {
-		outputTemp = val;
-	}
+    let outputTemp;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
 
-	let symbol: '°C' | '°F';
-	if (!outputType) {
-		symbol = inputType === 'cel' ? '°C' : '°F';
-	} else {
-		symbol = outputType === 'cel' ? '°C' : '°F';
-	}
+    let symbol: '°C' | '°F';
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
 
-	return `${outputTemp} ${symbol}`;
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 }
