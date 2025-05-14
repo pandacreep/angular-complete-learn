37fff984deb311d8bbfcb2162fc363265f7da2f7
Dmitriy Pak <dmt.alfa@gmail.com>
2025-05-14 14:18:00 +0500

    using custom DI tokens & providers

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-// import { TasksService } from './app/tasks/tasks.service';
+import { TasksService } from './app/tasks/tasks.service';
+import { InjectionToken } from '@angular/core';
 
-// bootstrapApplication(AppComponent, {
-//   providers: [TasksService],
-// }).catch((err) => console.error(err));
+export const TaskServiceToken = new InjectionToken<TasksService>('task-service-token');
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: TaskServiceToken, useClass: TasksService }],
+}).catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
