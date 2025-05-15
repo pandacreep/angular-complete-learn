4a96beefdd1df7d7c364170940da081979855520
Dmitriy Pak <dmt.alfa@gmail.com>
2025-05-15 09:45:44 +0500

    Initial commit for Making Sense of Change Detection - Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { CounterComponent } from './counter/counter.component';
+import { MessagesComponent } from './messages/messages.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [CounterComponent, MessagesComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  get debugOutput() {
+    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
+    return 'AppComponent Component Debug Output';
+  }
+}
