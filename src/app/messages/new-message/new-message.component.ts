669f6102ba3f443d80bfe71c143f4e340de40bbd
Dmitriy Pak <dmt.alfa@gmail.com>
2025-05-16 10:03:20 +0500

    using signals for sharing data accross components (with OnPush)

--- src/app/messages/new-message/new-message.component.ts
+++ src/app/messages/new-message/new-message.component.ts
@@ -1,5 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-new-message',
@@ -7,8 +8,10 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewMessageComponent {
+  private messageService = inject(MessagesService);
   add = output<string>();
   enteredText = signal('');
 
@@ -18,7 +21,8 @@ export class NewMessageComponent {
   }
 
   onSubmit() {
-    this.add.emit(this.enteredText());
+    // this.add.emit(this.enteredText());
+    this.messageService.addMessage(this.enteredText());
     this.enteredText.set('');
   }
 }
