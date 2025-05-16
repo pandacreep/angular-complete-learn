1c7ae2abd10564a040f9727887aaff797d32827b
Dmitriy Pak <dmt.alfa@gmail.com>
2025-05-16 15:59:28 +0500

    initial commit for RxJS (Observables) - deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+}
