7e1ba7e0a00841a45dc81adb8408ab6a4a549f4f
Dmitriy Pak <dmt.alfa@gmail.com>
2025-05-22 08:53:43 +0500

    configuring Http requests

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -18,11 +18,29 @@ export class AvailablePlacesComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: (resData) => console.log(resData.places), 
-    });
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .subscribe({
+        next: (resData) => {
+          console.log(resData.places);
+        },
+      });
+    // .get<{ places: Place[] }>('http://localhost:3000/places', {
+    //   // observe: 'response',
+    //   observe: 'events',
+    // })
+    // .subscribe({
+    //   // next: (response) => {
+    //   //   console.log(response);
+    //   //   console.log(response.body?.places);
+    //   // },
+    //   next: (event) => {
+    //     console.log(event);
+    //     // console.log(response.body?.places);
+    //   },
+    // });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
-    })
+    });
   }
 }
