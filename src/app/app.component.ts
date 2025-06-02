261babaa19b65c901e253df87e50b0a28b76db21
Dmitriy Pak <dmt.alfa@gmail.com>
2025-06-02 08:37:10 +0500

    Initial commit - Handling User Input & working with Forms (Template driven & Reactive)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+import { LoginComponent } from './auth/login/login.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [LoginComponent],
 })
 export class AppComponent {}
