abfa5f6fef35918c2d3603dac71f134ac8b00be3
pandacreep <dmitriy.pak@mail.ru>
2025-06-11 09:00:51 +0500

    Initial coomit section-14 routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent, UsersComponent],
 })
 export class AppComponent {}
