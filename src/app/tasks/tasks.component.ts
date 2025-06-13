d6ab081e9631dbb63a5aeff7b77a522646bc4567
pandacreep <dmitriy.pak@mail.ru>
2025-06-13 08:58:07 +0500

    loading data based on route parameters in child routes

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -12,5 +13,8 @@ import { Task } from './task/task.model';
 })
 export class TasksComponent {
   userId = input.required<string>();
-  userTasks: Task[] = [];
+  private taskService = inject(TasksService);
+  userTasks = computed(() =>
+    this.taskService.allTasks().filter((task) => task.userId === this.userId())
+  );
 }
