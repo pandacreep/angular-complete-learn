62e48fa36fd8f857184ef3e922e620718c5bd174
pandacreep <dmitriy.pak@mail.ru>
2025-06-16 09:03:14 +0500

    splitting route difinition acress multiple files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
+
+import { routes as userRoutes } from './users/users.route';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
@@ -13,26 +13,8 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix',
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
   },
-  // {
-  //   path: 'tasks', // <your-domain>/tasks
-  //   component: TasksComponent,
-  // },
   {
     path: '**',
     component: NotFoundComponent,
