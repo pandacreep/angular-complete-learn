5b38898540daeb89de79b8bb72a2a023dc4be19e
pandacreep <dmitriy.pak@mail.ru>
2025-06-18 08:50:18 +0500

    setting & resolving titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 
 import { routes as userRoutes } from './users/users.route';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { resolveUserTasks } from './tasks/tasks.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
+    title: 'No task selected',
   },
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
@@ -20,6 +22,7 @@ export const routes: Routes = [
     resolve: {
       userName: resolveUserName 
     },
+    title: resolveTitle,
   },
   {
     path: '**',
