34d927be4107d1a43fa91413e16bc16932a0a750
pandacreep <dmitriy.pak@mail.ru>
2025-06-18 09:03:22 +0500

    introducing Route Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,24 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 
 import { routes as userRoutes } from './users/users.route';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  resolveTitle,
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { resolveUserTasks } from './tasks/tasks.component';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -16,11 +30,12 @@ export const routes: Routes = [
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
     data: {
-      message: 'Hello!'
+      message: 'Hello!',
     },
     resolve: {
-      userName: resolveUserName 
+      userName: resolveUserName,
     },
     title: resolveTitle,
   },
