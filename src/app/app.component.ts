ce4407284aadfd8a5751c7e3ce3af14e3cc25026
pandacreep <dmitriy.pak@mail.ru>
2025-06-26 09:09:06 +0500

    Initial commit section 39 Standalone Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
