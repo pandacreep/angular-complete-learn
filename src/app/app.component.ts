37a6cbc1bce767b2b06cc16eceda83e8a435b2cc
pandacreep <dmitriy.pak@mail.ru>
2025-07-01 08:29:39 +0500

    creating a new Signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { DefaultComponent } from './default/default.component';
+import { SignalComponent } from './signals/signals.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: true,
-    imports: [DefaultComponent]
+    imports: [DefaultComponent, SignalComponent]
 })
 export class AppComponent {}
