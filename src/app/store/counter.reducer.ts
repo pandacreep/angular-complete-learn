8abdf6091f73f5d8dfd3287cde795d216a80842e
pandacreep <dmitriy.pak@mail.ru>
2025-07-04 09:07:16 +0500

    dispatching Actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,5 +1,9 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { increase } from './conter.actions';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+	initialState,
+	on(increase, (state) => state + 1),
+);
