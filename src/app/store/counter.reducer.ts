c94ca2f390497d0d7f58abf692ae990a54bcb94c
pandacreep <dmitriy.pak@mail.ru>
2025-07-23 08:16:24 +0500

    Handling Actions without Actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,7 +3,14 @@ import { increase } from './conter.actions';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-	initialState,
-	on(increase, (state, action) => state + action.value),
-);
+// export const counterReducer = createReducer(
+// 	initialState,
+// 	on(increase, (state, action) => state + action.value),
+// );
+
+export function counterReducer(state = initialState, action: any) {
+	if (action.type === '[Counter] Increment') {
+		return state + action.value;
+	}
+	return state;
+}
