a87675448688e5987bda4a0540f8e5c7a8ba9fd3
pandacreep <dmitriy.pak@mail.ru>
2025-07-24 08:29:46 +0500

    the Old @Effect & registering Effects

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,16 +1,20 @@
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './conter.actions';
-import { tap } from 'rxjs';
 
+@Injectable()
 export class CounterEffects {
-  saveCount = createEffect(() =>
-    this.actions$.pipe(
-      ofType(increment, decrement),
-      tap((action) => {
-        console.log(action);
-        localStorage.setItem('count', action.value.toString());
-      })
-    ), { dispatch: false }
+  saveCount = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(increment, decrement),
+        tap((action) => {
+          console.log(action);
+          localStorage.setItem('count', action.value.toString());
+        })
+      ),
+    { dispatch: false }
   );
 
   constructor(private actions$: Actions) {}
