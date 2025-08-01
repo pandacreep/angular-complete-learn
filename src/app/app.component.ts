dd2d3be91ca4ec1e53d19962023472444dab263d
pandacreep <dmitriy.pak@mail.ru>
2025-08-01 08:32:24 +0500

    Using custom components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ServerComponent } from './server/server.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 }
