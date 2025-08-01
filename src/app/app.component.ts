9dac9138a0924c97153b2b4bccac17bfec0cb1c0
pandacreep <dmitriy.pak@mail.ru>
2025-08-01 08:48:57 +0500

    working with Component Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,12 @@ import { ServerComponent } from './server/server.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
+  styles: `
+    h3 {
+      color: green;
+    }
+  `
 })
 export class AppComponent {
 }
