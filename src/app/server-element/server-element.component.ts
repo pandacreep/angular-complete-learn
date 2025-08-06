6af441fff06f4396b2c1bbc49fba569f73a2f1b6
pandacreep <dmitriy.pak@mail.ru>
2025-08-06 08:56:33 +0500

    More on View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent {
   @Input('srvElement')
