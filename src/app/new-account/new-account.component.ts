4b76716e08a073daf2f3f4e2ba84580263d91837
pandacreep <dmitriy.pak@mail.ru>
2025-08-26 08:43:28 +0500

    Using Services for Cross-Component Communication

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,8 +12,12 @@ export class NewAccountComponent {
   
   constructor(
     private loggingService: LoggingService,
-    private accountService: AccountsService
-  ) {}
+    private accountService: AccountsService,
+  ) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAcount(accountName, accountStatus);  
