385bd08423b66ce037effaeeeef9c55086673ea6
pandacreep <dmitriy.pak@mail.ru>
2025-09-05 08:42:29 +0500

    Subjects

---
+++ src/app/user.service.ts
@@ -0,0 +1,8 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+//   activatedEmmitter = new EventEmitter<boolean>();
+  activatedEmmitter = new Subject<boolean>();
+}
