97202c3f51e57bee99374ef9947d5714eb3d76ff
pandacreep <dmitriy.pak@mail.ru>
2025-09-08 08:32:03 +0500

    Init commit Forms (section 31)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+}
