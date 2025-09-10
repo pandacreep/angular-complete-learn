4e7c2f5819007da1f8267d4ffbe39ee49b9243b0
dmitriy.pak <dmt.alfa@gmail.com>
2025-09-10 09:27:47 +0500

    Reactive: Groupinh Control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,12 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('male')
-    })
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl('male'),
+    });
   }
 
   onSubmit(): void {
