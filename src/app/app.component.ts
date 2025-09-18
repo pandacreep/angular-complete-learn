7b17264f062ad0f1a7bb4824e79ea25772e53b18
pandacreep <dmitriy.pak@mail.ru>
2025-09-18 08:38:08 +0500

    Outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -26,7 +26,6 @@ export class AppComponent implements OnInit {
     .subscribe(
       responseData => {
         console.log(responseData);
-        
       }
     );
     console.log(postData);
@@ -58,6 +57,7 @@ export class AppComponent implements OnInit {
       )
       .subscribe((posts) => {
         console.log(posts);
+        this.loadedPosts = posts;
       });
   }
 }
