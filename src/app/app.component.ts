5b7ba270f23d1ecf38fcd9f24aef6800f0b301dd
pandacreep <dmitriy.pak@mail.ru>
2025-09-18 08:09:40 +0500

    GET ing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -29,10 +31,18 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+    .get('https://ng-complete-guide-b73c4-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+    .subscribe(posts => {
+      console.log(posts);
+    });
   }
 }
