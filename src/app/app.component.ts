6457aafac11e8f8075cca103ed51ac4925026eb1
pandacreep <dmitriy.pak@mail.ru>
2025-09-19 08:29:43 +0500

    Handling Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,21 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
     this.isFetching = false;
-    this.postsService.fetchPosts()
-      .subscribe(posts => {
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
         this.isFetching = false;
         this.loadedPosts = posts;
-      });
+      },
+      (error) => {
+        this.error = error.message;
+      }
+    );
   }
 
   onCreatePost(postData: Post) {
@@ -34,6 +39,10 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe((posts) => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
+      console.log(error);
+      
     });
   }
 
